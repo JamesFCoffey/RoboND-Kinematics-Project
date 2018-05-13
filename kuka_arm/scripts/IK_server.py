c77bdceb132a1e44c2f581210b3da5e97636efef
James Francis Coffey <james.francis.coffey@gmail.com>
2018-05-13 12:07:55 -0700

    Create symbols for forward kinematics

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -28,8 +28,11 @@ def handle_calculate_IK(req):
 
         ### Your FK code here
         # Create symbols
-	#
-	#
+        q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+        d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+        a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+        alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+
 	# Create Modified DH parameters
 	#
 	#
